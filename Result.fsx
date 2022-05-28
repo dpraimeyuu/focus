741801f91cf307d74e32e81b6ff0d80268fc806c
damian.plaza <damian.plaza@volue.com>
2022-05-28 22:47:58 +0200

    Working initial impl

---
+++ Result.fsx
@@ -0,0 +1,13 @@
+[<AutoOpen>]
+module Result =
+    let traverse (rs: Result<'a, 'b> array): Result<'a array, 'b> =
+        rs
+        |> List.ofArray
+        |> List.fold (fun rs' r ->
+            match rs', r with
+            | Ok rs', Ok r ->
+                Ok (r :: rs')
+            | Error rs', _ -> Error rs'
+            | _, Error r -> Error r
+        ) (Ok List.empty)
+        |> Result.map (Array.ofList)
--- script.fsx
+++ script.fsx
@@ -1,6 +1,7 @@
+#load "Result.fsx"
 module Domain =
     open System
-    
+    open Result
     module GitLog =
         type CommitSha = private CommitSha of sha: string
         type CommitAuthor = private CommitAuthor of author: string
@@ -8,27 +9,53 @@ module Domain =
         type FilePath = private FilePath of string
         type [<Measure>] addedLines
         type [<Measure>] removedLines
+        
+        let private isNumeric (s: string): bool = 
+            Int16.TryParse s
+            |> fst
+        type AddedLines =
+        | NotApplicable
+        | AddedLines of lines: int<addedLines>
+        with
+                static member From(linesNumber: string) =
+                    if linesNumber |> isNumeric
+                        then
+                            linesNumber
+                            |> int
+                            |> LanguagePrimitives.Int32WithMeasure
+                            |> AddedLines
+                        else
+                            NotApplicable
+        type RemovedLines =
+        | NotApplicable
+        | RemovedLines of lines: int<removedLines>
+            with
+                static member From(linesNumber: string) =
+                    if linesNumber |> isNumeric
+                        then
+                            linesNumber
+                            |> int
+                            |> LanguagePrimitives.Int32WithMeasure
+                            |> RemovedLines
+                        else
+                            NotApplicable
+
         type GitChange = private {
-            AddedLines: int<addedLines>
-            RemovedLines: int<removedLines>
+            AddedLines: AddedLines
+            RemovedLines: RemovedLines
             FilePath: FilePath
         } with
             static member From(rawChange: string): Result<GitChange, string> =
-                let rawChange = rawChange.Split("\r\t")
+                printfn $"single change {rawChange}"
+                let rawChange = rawChange.Split("\t")
                 match rawChange with
                 | [| addedLines; removedLines; file|] ->
                     Ok {
-                        AddedLines = 
-                            addedLines
-                            |> int
-                            |> LanguagePrimitives.Int32WithMeasure;
-                        RemovedLines =
-                            removedLines
-                            |> int
-                            |> LanguagePrimitives.Int32WithMeasure;
+                        AddedLines = AddedLines.From addedLines
+                        RemovedLines = RemovedLines.From removedLines
                         FilePath = FilePath file
                     }
-                | _ -> Error (sprintf "Error parsing git change %A for a single entry" rawChange)
+                | _ -> Error ($"Error parsing git change for a single entry")
 
         type GitLogEntry = private {
             CommitSha: CommitSha
@@ -37,13 +64,7 @@ module Domain =
             Changes: GitChange array
         }
         
-        let private splitEntryByLine (singleRawEntry: string) =
-            singleRawEntry.Split("\n")
-        type private GitLogRawEntry = GitLogRawEntry of rawEntry: string array
-        let private splitByEntry (gitLogContent: string): GitLogRawEntry array =
-            gitLogContent.Split("\n\n")
-            |> Array.map (splitEntryByLine >> GitLogRawEntry)
-
+        type GitLogRawEntry = GitLogRawEntry of rawEntry: string array
         type private GitLogEntryHeader = private {
             CommitSha: CommitSha
             Date: CommitDate
@@ -64,62 +85,71 @@ module Domain =
                     }
                     with
                         ex -> Error ($"Error while parsing a commit date of {rawHeader}.\nError message: {ex.Message}\nStack trace: {ex.StackTrace}")
-                | _ -> Error $"Wrong format of git entry header. Please check your `--pretty=format` argument of `git log` command"
+                | _ -> Error $"Wrong format of git entry header. Please check your `--pretty=format` argument of `git log` command: Raw header: {rawHeader}"
             
-        let private parseGitLogChanges (rawGitChanges: string array) =
-            rawGitChanges |> Array.map GitChange.From
-            
-        let private parseGitLogEntryHeader (rawHeader: string): Result<GitLogEntryHeader, string> =
-            GitLogEntryHeader.From rawHeader
+        let private isEntryHeader (rawEntryLine: string) = rawEntryLine.StartsWith("'--")
+        let private parseGitLogChanges = 
+            (Array.skipWhile isEntryHeader) >> (Array.map GitChange.From)
 
-        module Result =
-            let traverse (rs: Result<'a, 'b> array): Result<'a array, 'b> =
-                rs
-                |> List.ofArray
-                |> List.fold (fun rs' r ->
-                    match rs', r with
-                    | Ok rs', Ok r ->
-                        Ok (r :: rs')
-                    | Error rs', _ -> Error rs'
-                    | _, Error r -> Error r
-                ) (Ok List.empty)
-                |> Result.map (Array.ofList)
-        let private parseEntry (rawEntry: GitLogRawEntry): Result<GitLogEntry, string> =
+        let private parseGitLogHeaders = 
+            (Array.takeWhile isEntryHeader) >> (Array.map GitLogEntryHeader.From)
+
+        let private mergeEntryHeaderWithGitChanges gitChanges headerLine =
+            match headerLine, gitChanges with
+            | Ok header, Ok gitChanges ->
+                Ok {
+                    CommitSha = header.CommitSha;
+                    Author = header.Author;
+                    Date = header.Date;
+                    Changes = gitChanges
+                }
+            | Error header, _ -> Error header
+            | _, Error gitChanges -> Error gitChanges
+                    
+        let private parseEntry (rawEntry: GitLogRawEntry): Result<GitLogEntry array, string> =
             match rawEntry with
             | GitLogRawEntry rawEntry' ->
-                match rawEntry' |> List.ofArray with
-                | headerLine :: rawGitChanges ->
-                    let header = parseGitLogEntryHeader headerLine
-                    printfn "%A" rawGitChanges
-                    let gitChanges = 
-                        rawGitChanges
-                        |> Array.ofList
-                        |> parseGitLogChanges
-                        |> Result.traverse
 
+                let headerLines' =
+                    rawEntry' |> parseGitLogHeaders
+                let gitChanges = 
+                    rawEntry'
+                    |> parseGitLogChanges
+                    |> Result.traverse
 
-                    match header, gitChanges with
-                    | Ok header, Ok gitChanges ->
-                        Ok {
-                            CommitSha = header.CommitSha;
-                            Author = header.Author;
-                            Date = header.Date;
-                            Changes = gitChanges
-                        }
-                    | Error header, _ -> Error header
-                    | _, Error gitChanges -> Error gitChanges
-                | _ -> Error $"Error while parsing a single entry {rawEntry}"
+                let gitLogEntries = 
+                    headerLines'
+                    |> Array.map (mergeEntryHeaderWithGitChanges gitChanges)
+                    |> Result.traverse
+
+                gitLogEntries
 
         let private parseEntries (rawEntries: GitLogRawEntry array) =
             rawEntries
             |> Array.map parseEntry
             |> Result.traverse
+            |> Result.map (Array.collect id)
+
+        [<Literal>]
+        let private newLine = "\n"
+        [<Literal>]
+        let private emptyLine = newLine + newLine
+        
+        let private splitLinesWithinEntry (singleRawEntry: string) =
+            singleRawEntry.Split(newLine)
+
+        let splitByEntry (gitLogContent: string): GitLogRawEntry array =
+            gitLogContent.Split(emptyLine)
+            |> Array.map (splitLinesWithinEntry >> GitLogRawEntry)
+
+
         let parse (gitLogContent: string): Result<GitLogEntry array, string> =
             gitLogContent
             |> splitByEntry
             |> parseEntries
 
 open Domain
+open System
 
-let logFile = System.IO.File.ReadAllText(@"./logfile.log")
+let logFile = System.IO.File.ReadAllText(@"./logfile.log", Text.Encoding.UTF8)
 let logs = GitLog.parse logFile
